bd3ec4795d730e2c4c87d02adf4732cb56d5fe0b
rhanielfarias <rhanielfarias@gmail.com>
2022-07-16 16:02:42 -0300

    Criando um gerenciador de produto

---
+++ Exercício 1/Produto.java
@@ -0,0 +1,26 @@
+public class Produto {
+    private String nome;
+
+    public Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    private double preco;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+}
---
+++ Exercício 1/TesteProduto.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.List;
+public class TesteProduto {
+    public static void main(String[] args) {
+        List<Produto> listaDeProdutos = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
+        boolean continuar = true;
+        while (continuar) {
+            System.out.println("Bem-vindo ao gerenciador de produtos. Digite uma opção");
+            System.out.println("1 - Adicionar um produto");
+            System.out.println("2 - Ver a lista de produtos já adicionados");
+            System.out.println("3 - Excluir um produto pelo nome");
+            System.out.println("4 - Sair do gerenciador de produtos");
+            int opcao = input.nextInt();
+            input.nextLine();
+            switch (opcao) {
+                case 1:
+                    System.out.println("Informe o nome do produto");
+                    String nome = input.nextLine();
+                    System.out.println("Informe o valor do produto");
+                    double preco = input.nextDouble();
+                    Produto produto = new Produto(nome, preco);
+                    listaDeProdutos.add(produto);
+                    break;
+                case 2:
+                    for (int i = 0; i < listaDeProdutos.size(); i++) {
+                        Produto verProduto = listaDeProdutos.get(i);
+                        System.out.println("Os produtos que já estão salvos são os seguintes:" + verProduto.getNome());
+                        System.out.print(" " + verProduto.getPreco());
+                    }
+                    break;
+                case 3:
+                    System.out.println("Informe o nome do produto a remover");
+                    String nomeRemover = input.nextLine();
+                    for (int i = 0; i < listaDeProdutos.size(); i++) {
+                        Produto verProduto = listaDeProdutos.get(i);
+                        if (nomeRemover.equals(verProduto.getNome())) {
+                            listaDeProdutos.remove(verProduto);
+                        }
+                    }
+                    break;
+                case 4:
+                    continuar = false;
+                    break;
+                default:
+                    System.out.println("Opção inválida");
+            }
+        }
+
+    }
+}
